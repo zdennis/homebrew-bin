1d04eba6f17fb4c2f2f712c1b63f552f14fbac33
Zach Dennis <zach.dennis@gmail.com>
2026-02-07 13:13:47 -0500

    Use git tag for ascii-banner versioning

--- Formula/ascii-banner.rb
+++ Formula/ascii-banner.rb
@@ -1,9 +1,9 @@
 class AsciiBanner < Formula
   desc "Create ASCII art banners with color, sizing, and alignment options"
   homepage "https://github.com/zdennis/bin"
-  url "https://raw.githubusercontent.com/zdennis/bin/master/bin/ascii-banner"
   version "1.0.0"
-  sha256 "2ae8f88fd91b5932ec405b56a5cbe069682fcb593459071bbd47fbec93c31810"
+  url "https://raw.githubusercontent.com/zdennis/bin/ascii-banner-v#{version}/bin/ascii-banner"
+  sha256 "6b29d4b246c22e473918907a578ec34427b07b89d13799f3b7550f4e98f4f84c"
   license "MIT"
 
   depends_on "ruby"
