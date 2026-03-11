fb586dd6b27ab83193d0738769f778695c9b37ed
Zach Dennis <zach.dennis@gmail.com>
2026-03-11 09:22:49 -0400

    Update workspace to v0.11.0
    Updated to latest release.
    
    Source: https://github.com/zdennis/workspace

--- Formula/workspace.rb
+++ Formula/workspace.rb
@@ -1,8 +1,8 @@
 class Workspace < Formula
   desc "Manage tmuxinator-based development workspaces in iTerm2"
   homepage "https://github.com/zdennis/workspace"
-  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "6abf0b4fcf869629f921fa3de93d552577f896e6fc6f0e142aadd914c349c729"
+  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "5b71e09ee7c5a5db42e3efdc3dd26d4feea7af885523ebb9bd90dd2a8cbb4f7a"
   license "MIT"
 
   depends_on :macos
--- Formula/zdennis-bin-all.rb
+++ Formula/zdennis-bin-all.rb
@@ -1,8 +1,8 @@
 class ZdennisBinAll < Formula
   desc "Install all zdennis/bin tools"
   homepage "https://github.com/zdennis/bin"
-  url "https://raw.githubusercontent.com/zdennis/bin/zdennis-bin-all-v1.17.0/bin/zdennis-bin-all"
-  sha256 "c8ed09dd95dc6565d51e4a4da204af5900032ea9cd44829d66c27b7fe38ac47f"
+  url "https://raw.githubusercontent.com/zdennis/bin/zdennis-bin-all-v1.18.0/bin/zdennis-bin-all"
+  sha256 "85fa995941d73499023c2420bc4480ffa85954094c6a0bbd9259e51d10aead25"
   license "MIT"
 
   depends_on "zdennis/bin/alias-directory"
