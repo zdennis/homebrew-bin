3ee748ca525e0a9b75640454a6180bcda4d7ced9
Zach Dennis <zach.dennis@gmail.com>
2026-03-30 08:07:42 -0400

    Update workspace to v0.12.2
    Updated to latest release. Also fixed brew-formula-helper path.
    
    Source: https://github.com/zdennis/workspace

--- Formula/workspace.rb
+++ Formula/workspace.rb
@@ -1,8 +1,8 @@
 class Workspace < Formula
   desc "Manage tmuxinator-based development workspaces in iTerm2"
   homepage "https://github.com/zdennis/workspace"
-  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "ff0276dfeb0423b6fd989a526a87260d0d2d981705195ae0c691e05cef1be78d"
+  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "173c0b81989fe623db9d0d7c0a2062d7864db9adbe1c22f86c6421924df2ef7a"
   license "MIT"
 
   depends_on :macos
--- Formula/zdennis-bin-all.rb
+++ Formula/zdennis-bin-all.rb
@@ -1,8 +1,8 @@
 class ZdennisBinAll < Formula
   desc "Install all zdennis/bin tools"
   homepage "https://github.com/zdennis/bin"
-  url "https://raw.githubusercontent.com/zdennis/bin/zdennis-bin-all-v1.22.0/bin/zdennis-bin-all"
-  sha256 "3feff60799544c1aeb69de3f2b1c316cf52430c19eea27d40063d5f15ab6f2e8"
+  url "https://raw.githubusercontent.com/zdennis/bin/zdennis-bin-all-v1.22.1/bin/zdennis-bin-all"
+  sha256 "ef7b5b14f977fb1786278f81dfd3c047e5f2c30ce2fbd39dfe155e5edf6ef7dc"
   license "MIT"
 
   depends_on "zdennis/bin/alias-directory"
