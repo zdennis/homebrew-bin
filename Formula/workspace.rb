38765fa933921c50e48b99bb8d975551ad02e71d
Zach Dennis <zach.dennis@gmail.com>
2026-08-09 12:23:12 -0400

    feat(formula): update workspace to v0.21.0

--- Formula/workspace.rb
+++ Formula/workspace.rb
@@ -1,8 +1,8 @@
 class Workspace < Formula
   desc "Manage tmuxinator-based development workspaces in iTerm2"
   homepage "https://github.com/zdennis/workspace"
-  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "d36581947545ce7fca002bbaa690d25d5d7c5fec5ac4d6556c2263545e296b8d"
+  url "https://github.com/zdennis/workspace/archive/refs/tags/v0.21.0.tar.gz"
+  sha256 "87ba5f731e3fcd2acee9a97d0aa296ea83039b8519c7ec881e5f1709f7343b4d"
   license "MIT"
 
   depends_on :macos
